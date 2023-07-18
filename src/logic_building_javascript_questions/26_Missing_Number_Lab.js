8925e6740ffa46b01f163c3ab9c071399e5ffea4
shubham2o <shubhamsharma.ha@gmail.com>
2023-07-18 09:28:26 +0530

    26_Missing_Number_Lab.js inside src/logic_building_javascript_questions added

--- src/logic_building_javascript_questions/26_Missing_Number_Lab.js
+++ src/logic_building_javascript_questions/26_Missing_Number_Lab.js
@@ -3,6 +3,13 @@
 console.log(`26. 
 Missing Number Lab`);
 
-function missingNumberLab() { };
+function missingNumberLab(nums) {
+    let expectedSum = (nums.length * (nums.length + 1)) / 2;
+    let actualSum = nums.reduce((a, b) => a + b, 0);
 
-missingNumberLab();
+    console.log(expectedSum - actualSum);
+};
+
+missingNumberLab([3, 0, 1]);
+missingNumberLab([0, 1]);
+missingNumberLab([9, 6, 4, 2, 3, 5, 7, 0, 1]);
